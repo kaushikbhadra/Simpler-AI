a6b57e5643c3dcf0d818669c348f7d21e0e40e9a
Kaushik Bhadra <kbhadra100@gmail.com>
2023-08-19 20:33:47 +0530

    setup dashboard page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,14 @@
-import { UserButton } from '@clerk/nextjs'
+'use client'
+// import { UserButton } from '@clerk/nextjs'
 import MobileSidebar from '@/components/mobile-sidebar'
 
 const navbar = () => {
   return (
     <div className='flex items-center p-4'>
       <MobileSidebar />
-      <div className='flex w-full justify-end'>
+      {/* <div className='flex w-full justify-end'>
         <UserButton afterSignOutUrl='/' />
-      </div>
+      </div> */}
     </div>
   )
 }
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -71,7 +71,7 @@ const Sidebar = () => {
       <div className='px-3 py-2 flex-1'>
         <Link href='/dashboard' className='flex items-center pt-3 mb-14'>
           <div className='relative w-8 h-8 mr-4'>
-            <Image fill alt='Logo' src='/logo.png' />
+            <Image fill alt='Logo' src='/logo.png' sizes='32' />
           </div>
           <h1 className={cn('text-2xl font-bold', montserrat.className)}>
             Simpler
