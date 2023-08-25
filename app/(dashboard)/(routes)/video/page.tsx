53fac8f2c0a9df03f05d71826f17cee0812736fb
Kaushik Bhadra <kbhadra100@gmail.com>
2023-08-25 20:33:21 +0530

    adding video generation page

--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { Music } from 'lucide-react'
+import { VideoIcon } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
 import * as z from 'zod'
@@ -16,9 +16,9 @@ import Heading from '@/components/heading'
 import { formSchema } from './constants'
 import { Loader } from '@/components/loader'
 
-const MusicPage = () => {
+const VideoPage = () => {
   const router = useRouter()
-  const [music, setMusic] = useState<string>()
+  const [video, setVideo] = useState<string>()
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -31,10 +31,10 @@ const MusicPage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      setMusic(undefined)
-      const response = await axios.post('/api/music', values)
+      setVideo(undefined)
+      const response = await axios.post('/api/video', values)
 
-      setMusic(response.data.audio)
+      setVideo(response.data[0])
 
       form.reset()
     } catch (error) {
@@ -47,11 +47,11 @@ const MusicPage = () => {
   return (
     <div>
       <Heading
-        title='Music Generation'
-        description='Turn your prompt into music.'
-        icon={Music}
-        iconColor='text-emerald-500'
-        bgColor='bg-emerald-500/10'
+        title='Video Generation'
+        description='Turn your prompt into video.'
+        icon={VideoIcon}
+        iconColor='text-orange-700'
+        bgColor='bg-orange-700/10'
       />
       <div className='px-4 lg:px-8'>
         <div>
@@ -68,7 +68,7 @@ const MusicPage = () => {
                       <Input
                         className='border-0 p-2 outline-none focus-visible:ring-0 focus-visible:ring-transparent'
                         disabled={isLoading}
-                        placeholder='Guitar with flute mix?'
+                        placeholder='Crow dancing on pond.'
                         {...field}
                       />
                     </FormControl>
@@ -92,15 +92,18 @@ const MusicPage = () => {
               <Loader />
             </div>
           )}
-          {!music && !isLoading && <Empty label='No music generated.' />}
-          {music && (
-            <audio controls className='w-full mt-8'>
-              <source src={music} />
-            </audio>
+          {!video && !isLoading && <Empty label='No video generated.' />}
+          {video && (
+            <video
+              controls
+              className='w-full aspect-video mt-8 rounded-lg border bg-black'
+            >
+              <source src={video} />
+            </video>
           )}
         </div>
       </div>
     </div>
   )
 }
-export default MusicPage
+export default VideoPage
