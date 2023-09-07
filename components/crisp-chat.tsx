7ce48d50e91883acf2d28321dd5c4735be91e524
Kaushik Bhadra <31278021+kaushikbhadra@users.noreply.github.com>
2023-09-08 00:59:02 +0530

    Update crisp-chat.tsx
    fix crisp chat error

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from 'crisp-sdk-web'
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure(`${process.env.CRISP_CHAT_ID}`)
+    Crisp.configure("273cb0d9-061b-4942-b7d0-8d8b32b196f7")
   }, [])
 
   return null
